b2c321b778566b0248d6968658b3373aa7877ad0
Moyaj Bin Alamgir <130137627+MOYAJ-BIN-ALAMGIR@users.noreply.github.com>
2025-08-21 15:01:38 +0600

    The ‘controller’ & ‘action’ tokens in Attribute Routing 2

---
+++ AttributeRouting/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AttributeRouting.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Route("[controller]/CallMe")]
+        public string Index()
+        {
+            return "'Admin' Controller, 'Index' View";
+        }
+
+        public string List()
+        {
+            return "'Admin' Controller, 'List' View";
+        }
+    }
+}
